565ac16dc57a04f925d5c27cb71d623231768620
Glauber Donizeti Gasparotto Junior <juniorgasparotto@msn.com>
2020-02-17 01:50:44 -0300

    certificate kafka

--- src/KafkaLearning.Web/ClientApp/src/environments/environment.prod.ts
+++ src/KafkaLearning.Web/ClientApp/src/environments/environment.prod.ts
@@ -2,13 +2,13 @@ export const environment = {
   production: true,
   kafka: {
     consumerDefault: {
-      bootstrapServers: "my-cluster-kafka-bootstrap:9092",
+      bootstrapServers: "my-cluster-kafka-bootstrap-project-kafka.192.168.0.10.nip.io:443",
       enableAutoCommit: true,
       autoOffSetReset: 0,
       enablePartitionEof: true
     },
     producerDefault: {
-      bootstrapServers: "my-cluster-kafka-bootstrap:9092",
+      bootstrapServers: "my-cluster-kafka-bootstrap-project-kafka.192.168.0.10.nip.io:443",
       topic: "Chat"
     }
   }
